50e236d648bf6f240747b60f1dd3fda261eed223
ShaneMarusczak <marusczaks@gmail.com>
2020-07-23 17:21:04 -0400

    modal on its own

--- buildingBlocks.js
+++ buildingBlocks.js
@@ -9,6 +9,44 @@ function alertModalControl(message, duration) {
 	});
 }
 
+function modal(message, duration) {
+	const modalBox = document.createElement("div");
+	const innerModalBox = document.createElement("div");
+	const modalMessage = document.createElement("span");
+	innerModalBox.appendChild(modalMessage);
+	modalBox.appendChild(innerModalBox);
+	modalBox.style = `
+	z-index: 1;
+	position: fixed;
+	padding: 0;
+	margin: 0;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background: rgba(0, 0, 0, 0.6);
+	`;
+	innerModalBox.style = `
+	text-align: center;
+	border-radius: 5px;
+	box-shadow: inset 0px 0px 0px 4px black;
+	margin-left: 40vw;
+	margin-top: 10vh;
+	background-color: azure;
+	width: 350px;
+	height: 15vh;
+	`;
+	modalMessage.style = `
+	vertical-align: middle;
+	font-family: "Orbitron", sans-serif;
+	font-size: xx-large;
+	line-height: 15vh;
+	`;
+	modalMessage.innerText = message;
+	document.getElementsByTagName("html")[0].appendChild(modalBox);
+	sleep(duration).then(() => modalBox.remove());
+}
+
 function randomIntFromInterval(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
