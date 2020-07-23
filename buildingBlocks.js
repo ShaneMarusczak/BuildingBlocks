efdf077a40093f4fba9a5d6a114a5adad3c9abdd
ShaneMarusczak <marusczaks@gmail.com>
2020-07-23 14:51:21 -0400

    tryingsomethingdifferent

--- buildingBlocks.js
+++ buildingBlocks.js
@@ -1,43 +1,41 @@
 "use strict";
-class BuildingBlocks {
-	BuildingBlocks() {
-		this.alertModalControl = function (message, duration) {
-			document.getElementById("alertshader").style.display = "block";
-			document.getElementById("alertmessage").innerText = message;
-			window.sleep(duration).then(() => {
-				document.getElementById("alertshader").style.display = "none";
-			});
-		};
 
-		this.randomIntFromInterval = function (min, max) {
-			return Math.floor(Math.random() * (max - min + 1) + min);
-		};
-
-		this.sleep = function (ms) {
-			return new Promise((resolve) => setTimeout(resolve, ms));
-		};
-
-		this.setCookie = function (cname, cvalue, exdays) {
-			var d = new Date();
-			d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-			var expires = "expires=" + d.toUTCString();
-			document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-		};
-
-		this.getCookie = function (cname) {
-			var name = cname + "=";
-			var decodedCookie = decodeURIComponent(document.cookie);
-			var ca = decodedCookie.split(";");
-			for (var i = 0; i < ca.length; i++) {
-				var c = ca[i];
-				while (c.charAt(0) == " ") {
-					c = c.substring(1);
-				}
-				if (c.indexOf(name) == 0) {
-					return c.substring(name.length, c.length);
-				}
-			}
-			return "";
-		};
-	}
+function alertModalControl(message, duration) {
+	document.getElementById("alertshader").style.display = "block";
+	document.getElementById("alertmessage").innerText = message;
+	window.sleep(duration).then(() => {
+		document.getElementById("alertshader").style.display = "none";
+	});
 }
+
+function randomIntFromInterval(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function sleep(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function setCookie(cname, cvalue, exdays) {
+	var d = new Date();
+	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+	var expires = "expires=" + d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+	var name = cname + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(";");
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == " ") {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
