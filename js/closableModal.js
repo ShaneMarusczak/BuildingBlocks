b6de42c8a1bb5605ea83e9bcf3bec2260cf43807
ShaneMarusczak <marusczaks@gmail.com>
2020-07-24 16:41:38 -0400

    closable modal

--- js/closableModal.js
+++ js/closableModal.js
@@ -1,16 +1,22 @@
 /*eslint-disable no-unused-vars */
 "use strict";
 
-function modal(message, duration) {
+function closableModal(message) {
 	const modalBox = document.createElement("div");
 	modalBox.id = "modal-box";
 	const innerModalBox = document.createElement("div");
 	innerModalBox.id = "inner-modal-box";
 	const modalMessage = document.createElement("span");
 	modalMessage.id = "modal-message";
+	const closeButton = document.createElement("span");
+	closeButton.id = "close-button";
+	closeButton.innerHTML = "&times;";
 	innerModalBox.appendChild(modalMessage);
+	innerModalBox.appendChild(closeButton);
 	modalBox.appendChild(innerModalBox);
 	modalMessage.innerText = message;
 	document.getElementsByTagName("html")[0].appendChild(modalBox);
-	window.sleep(duration).then(() => modalBox.remove());
+	closeButton.addEventListener("click", () => {
+		modalBox.remove();
+	});
 }
